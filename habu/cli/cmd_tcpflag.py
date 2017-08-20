c23b1349f914fa811b0c6f0fca93315895083242
Fabian Martinez Portantier <fportantier@securetia.com>
2017-08-19 22:13:14 -0300

    more options

--- habu/cli/cmd_tcpflag.py
+++ habu/cli/cmd_tcpflag.py
@@ -1,23 +1,29 @@
 import click
-from scapy.all import ICMP, IP, conf, sr, TCP
+from scapy.all import ICMP, IP, conf, sr, sr1, TCP
 
 
 @click.command()
 @click.argument('ip')
 @click.option('-p', 'port', default=80, help='Port to use (default: 80)')
+@click.option('-f', 'flags', default=None, help='Flags to use (default: fuzz with all flags)')
+@click.option('-r', 'rflags', default=None, help='Filter by response flags (default: show all responses)')
 @click.option('-v', 'verbose', is_flag=True, default=False, help='Verbose')
-def cmd_tcpflag(ip, port, verbose):
+def cmd_tcpflag(ip, port, flags, rflags, verbose):
 
-    conf.verb = verbose
+    conf.verb = False
 
-    pkts = IP(dst=ip) / TCP(flags=(0, 255), dport=port)
+    if not flags:
+        flags = (0, 255)
 
-    ans, unans = sr(pkts, timeout=2)
+    pkts = IP(dst=ip) / TCP(flags=flags, dport=port)
 
     out = "{:>8} -> {:<8}"
 
-    for s,r in ans:
-        print(out.format(s.sprintf(r"%TCP.flags%"), r.sprintf(r"%TCP.flags%")))
+    for pkt in pkts:
+        ans = sr1(pkt, timeout=0.2)
+        if ans:
+            if not rflags or all(i in ans.sprintf(r"%TCP.flags%") for i in rflags):
+                print(out.format(pkt.sprintf(r"%TCP.flags%"), ans.sprintf(r"%TCP.flags%")))
 
     return True
 
