9775a864443e1f6a1f55ebd1045d863dd93c91e2
Fabian Martinez Portantier <fportantier@securetia.com>
2017-08-30 01:19:07 -0300

    isn with graphics

--- habu/cli/cmd_isn.py
+++ habu/cli/cmd_isn.py
@@ -1,6 +1,8 @@
 import click
-from scapy.all import ICMP, IP, conf, sr1, UDP, TCP, srloop
+import matplotlib.pyplot as plt
 
+from scapy.all import ICMP, IP, conf, sr1, UDP, TCP, srloop, send, RandShort
+from time import sleep
 
 @click.command()
 @click.argument('ip')
@@ -15,6 +17,24 @@ def cmd_isn(ip, port, count, iface, verbose):
     if iface:
         conf.iface = iface
 
+    isn_values = []
+
+    for _ in range(count):
+        pkt = IP(dst=ip)/TCP(sport=RandShort(), dport=port, flags="S")
+        ans = sr1(pkt, timeout=0.5)
+        if ans:
+            send(IP(dst=ip)/TCP(sport=pkt[TCP].sport, dport=port, ack=ans[TCP].seq + 1, flags='A'))
+            isn_values.append(ans[TCP].seq)
+            ans.show2()
+        #sleep(1)
+
+    plt.plot(range(len(isn_values)), isn_values, 'ro')
+    plt.show()
+
+    #, count=count, verbose=verbose, inter=0.1)
+
+
+    '''
     ans, unans = srloop(IP(dst=ip)/TCP(dport=port, flags="S"), count=count, verbose=verbose, inter=0.1)
 
     if verbose:
@@ -22,10 +42,18 @@ def cmd_isn(ip, port, count, iface, verbose):
             print(r.summary())
 
     temp = 0
+    isn_values = []
 
     for s,r in ans:
         temp = r[TCP].seq - temp
         print("%d\t%+d" %(r[TCP].seq, temp))
+        isn_values.append(r[TCP].seq)
+
+    plt.plot(range(len(isn_values)), isn_values, 'ro')
+    plt.show()
+
+    #plot_scatter(f=None, xs=range(len(isn_values)), ys=isn_values, size=100, pch='x', colour='#333', title='asd')
+    '''
 
     return True
 
