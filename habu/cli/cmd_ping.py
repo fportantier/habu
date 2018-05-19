e54da7bd841dc4b9c016699537d1e8eef19945e0
Fabian Martinez Portantier <fabian@portantier.com>
2018-05-19 15:11:41 -0300

    fixed ping to loopback and new option -i (interface)

--- habu/cli/cmd_ping.py
+++ habu/cli/cmd_ping.py
@@ -5,18 +5,23 @@ import click
 
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-from scapy.all import ICMP, IP, conf, sr1
+from scapy.all import ICMP, IP, conf, sr1, L3RawSocket
 
 
 @click.command()
 @click.argument('ip')
+@click.option('-i', 'interface', default=None, help='Wich interface to use (default: auto)')
 @click.option('-c', 'count', default=0, help='How many packets send (default: infinit)')
 @click.option('-t', 'timeout', default=2, help='Timeout in seconds (default: 2)')
 @click.option('-w', 'wait', default=1, help='How many seconds between packets (default: 1)')
 @click.option('-v', 'verbose', is_flag=True, default=False, help='Verbose')
-def cmd_ping(ip, count, timeout, wait, verbose):
+def cmd_ping(ip, interface, count, timeout, wait, verbose):
+
+    if interface:
+        conf.iface = interface
 
     conf.verb = False
+    conf.L3socket=L3RawSocket
 
     layer3 = IP()
     layer3.dst = ip
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='habu',
-    version='0.0.64',
+    version='0.0.65',
     description='Python Network Hacking Toolkit',
     long_description=readme,
     long_description_content_type='text/markdown',
