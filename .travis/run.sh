6734cce59a97c5a26dad8398dd2c37f623a90b57
Fabian Martinez Portantier <fabian@portantier.com>
2018-07-27 18:09:38 -0300

    travis dir

---
+++ .travis/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pip install -e .
+
+for cmd in $(whereis habu | tr " " "\n" | grep "habu\." | grep -v "/usr/")
+do
+    echo $cmd
+    $cmd --help > /dev/null
+    if [ "$?" != "0" ]
+    then
+        echo "Error running help for $cmd"
+        exit 1
+    fi
+done
+
