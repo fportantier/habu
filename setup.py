362dc90679fbb091fc6a213bf6a7fdee6d39159d
Fabian Martinez Portantier <fabian@portantier.com>
2018-10-14 20:34:36 -0300

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='habu',
-    version='0.0.83',
+    version='0.0.84',
     description='Python Network Hacking Toolkit',
     long_description=readme,
     long_description_content_type='text/markdown',
