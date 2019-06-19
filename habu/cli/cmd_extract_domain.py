5254517f09e31a2a9f7c770217f03634183cdd79
Fabian Martinez Portantier <fabian@portantier.com>
2019-06-18 22:45:17 -0300

    new command habu.extract.domain

---
+++ habu/cli/cmd_extract_domain.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import socket
+import tldextract
+
+import click
+import regex as re
+
+from habu.lib import libdns
+
+
+def extract_domain(data):
+
+    regexp = re.compile(r"([a-zA-Z0-9_.-]+)")
+
+    match = regexp.finditer(data)
+
+    result = set()
+
+    for m in match:
+
+        candidate = m.group(0).lower()
+
+        if '.' not in candidate:
+            continue
+
+        if not re.match('[a-z]+', candidate):
+            continue
+
+        if not re.match('[a-z0-9]+\.[a-z0-9]', candidate):
+            continue
+
+        tld = tldextract.extract(candidate)
+        if tld.suffix:
+            result.add(tld.domain + '.' + tld.suffix.rstrip('.'))
+
+    return list(result)
+
+
+@click.command()
+@click.argument('infile', type=click.File('r'), default='-')
+@click.option('-c', 'check', is_flag=True, default=False, help='Check if domain has NS servers defined')
+@click.option('-v', 'verbose', is_flag=True, default=False, help='Verbose output')
+@click.option('-j', 'jsonout', is_flag=True, default=False, help='JSON output')
+def cmd_extract_domain(infile, check, verbose, jsonout):
+    """Extract valid domains from a file or stdin.
+
+    Optionally, check each domain for the presence of NS registers.
+
+    Example:
+
+    \b
+    $ cat /var/log/some.log | habu.extract.domain -c
+    google.com
+    ibm.com
+    redhat.com
+    """
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    data = infile.read()
+
+    result = extract_domain(data)
+
+    if check:
+        logging.info('Checking against DNS...')
+        result = [ domain for domain in result if libdns.ns(domain) ]
+
+    if jsonout:
+        print(json.dumps(result, indent=4))
+    else:
+        print('\n'.join(result))
+
+
+if __name__ == '__main__':
+    cmd_extract_domain()
---
+++ habu/cli/cmd_shodan_query.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import os
+import sys
+
+import click
+
+from habu.lib.loadcfg import loadcfg
+from habu.lib.shodan import shodan_query
+
+@click.command()
+@click.argument('query')
+@click.option('-c', 'no_cache', is_flag=True, default=False, help='Disable cache')
+@click.option('-v', 'verbose', is_flag=True, default=False, help='Verbose output')
+@click.option('-o', 'output', type=click.File('w'), default='-', help='Output file (default: stdout)')
+def cmd_shodan_query(query, no_cache, verbose, output):
+    """Simple shodan API client.
+
+    Prints the JSON result of a shodan query.
+
+    Example:
+
+    \b
+    $ habu.shodan 8.8.8.8
+    {
+        "hostnames": [
+            "google-public-dns-a.google.com"
+        ],
+        "country_code": "US",
+        "org": "Google",
+        "data": [
+            {
+                "isp": "Google",
+                "transport": "udp",
+                "data": "Recursion: enabled",
+                "asn": "AS15169",
+                "port": 53,
+                "hostnames": [
+                    "google-public-dns-a.google.com"
+                ]
+            }
+        ],
+        "ports": [
+            53
+        ]
+    }
+    """
+
+    habucfg = loadcfg()
+
+    if 'SHODAN_APIKEY' not in habucfg:
+        print('You must provide a shodan apikey. Use the ~/.habu.json file (variable SHODAN_APIKEY), or export the variable HABU_SHODAN_APIKEY')
+        print('Get your API key from https://www.shodan.io/')
+        sys.exit(1)
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    data = shodan_query(query, habucfg['SHODAN_APIKEY'], no_cache, verbose)
+
+    output.write(json.dumps(data, indent=4))
+    output.write('\n')
+
+if __name__ == '__main__':
+    cmd_shodan_query()
