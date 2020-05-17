cd9f976380a68678bdd3afc4cc28914f7ac86498
Fabian Martinez Portantier <fportantier@securetia.com>
2020-05-17 17:19:14 -0300

    moved to icmp namespace

--- habu/cli/cmd_icmp_ping.py
+++ habu/cli/cmd_icmp_ping.py
@@ -17,11 +17,11 @@ from scapy.all import ICMP, IP, conf, sr1, L3RawSocket
 @click.option('-t', 'timeout', default=2, help='Timeout in seconds (default: 2)')
 @click.option('-w', 'wait', default=1, help='How many seconds between packets (default: 1)')
 @click.option('-v', 'verbose', is_flag=True, default=False, help='Verbose')
-def cmd_ping(ip, interface, count, timeout, wait, verbose):
+def cmd_icmp_ping(ip, interface, count, timeout, wait, verbose):
     """The classic ping tool that send ICMP echo requests.
 
     \b
-    # habu.ping 8.8.8.8
+    # habu.icmp.ping 8.8.8.8
     IP / ICMP 8.8.8.8 > 192.168.0.5 echo-reply 0 / Padding
     IP / ICMP 8.8.8.8 > 192.168.0.5 echo-reply 0 / Padding
     IP / ICMP 8.8.8.8 > 192.168.0.5 echo-reply 0 / Padding
@@ -74,4 +74,4 @@ def cmd_ping(ip, interface, count, timeout, wait, verbose):
     return True
 
 if __name__ == '__main__':
-    cmd_ping()
+    cmd_icmp_ping()
