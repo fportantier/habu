15f4ad7cea06e0709789f2fb06290fd4d3f0df55
Fabian Martinez Portantier <fportantier@securetia.com>
2020-05-17 17:14:56 -0300

    moved to ip namespace and renamed to habu.ip.public

--- habu/cli/cmd_ip_public.py
+++ habu/cli/cmd_ip_public.py
@@ -10,7 +10,7 @@ from habu.lib.ip import get_public_ipv4, get_public_ipv6
 @click.option('-4', '--ipv4', 'ipv4', is_flag=True, default=False, help='Print your public IPv4 address (default)')
 @click.option('-6', '--ipv6', 'ipv6', is_flag=True, default=False, help='Print your public IPv6 address')
 @click.option('-j', '--json', 'json_output', is_flag=True, default=False, help='Print the output in JSON format')
-def cmd_ip(ipv4, ipv6, json_output):
+def cmd_ip_public(ipv4, ipv6, json_output):
     """Get the public IP address of the connection from https://api.ipify.org.
 
     Example:
@@ -38,4 +38,4 @@ def cmd_ip(ipv4, ipv6, json_output):
 
 
 if __name__ == '__main__':
-    cmd_ip()
+    cmd_ip_public()
