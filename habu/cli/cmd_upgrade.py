af642ca020f6ee8761026a6edf1dfbebbb75ff63
Fabian Martinez Portantier <fportantier@securetia.com>
2020-08-19 23:50:40 -0300

    unify cmd_clean and cmd_upgrade

--- habu/cli/cmd_upgrade.py
+++ habu/cli/cmd_upgrade.py
@@ -1,7 +1,32 @@
 import logging
+import os
 import subprocess
+from pathlib import Path
 
 import click
+import pkg_resources
+
+
+def find_console_scripts(package_name):
+    entrypoints = [ ep.name for ep in pkg_resources.iter_entry_points('console_scripts') if ep.module_name.startswith(package_name) ]
+    return entrypoints
+
+
+def clean_old_entrypoints():
+    """Clean old habu entrypoints."""
+
+    logging.basicConfig(level=logging.INFO)
+
+    entrypoints = find_console_scripts('habu')
+
+    for dirname in os.environ['PATH'].split(':'):
+        for filename in Path(dirname).glob('habu.*'):
+            if filename.name not in entrypoints:
+                try:
+                    filename.unlink()
+                    logging.info(f'Deleted entrypoint {filename}')
+                except Exception as e:
+                    logging.error(f'Error deleting {filename}: {e}')
 
 
 @click.command()
@@ -22,9 +47,7 @@ def cmd_upgrade():
     p = subprocess.Popen(command)
     p.wait()
 
-    p = subprocess.Popen(['habu.clean'])
-    p.wait()
-
+    clean_old_entrypoints()
 
 if __name__ == '__main__':
     cmd_upgrade()
