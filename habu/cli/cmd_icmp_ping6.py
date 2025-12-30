c0337a687b1af9d4ce74d4be8c144f4bf6edca37
Fabian Martinez Portantier <fportantier@securetia.com>
2025-12-30 13:32:29 -0300

    new icmp.ping6 command

---
+++ habu/cli/cmd_icmp_ping6.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import logging
+from time import sleep
+
+import click
+from scapy.all import ICMPv6EchoRequest, IPv6, conf, ls, sr1
+
+from habu.lib.run_as_root import run_as_root
+
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+
+@click.command()
+@click.argument("ip")
+@click.option(
+    "-i", "interface", default=None, help="Wich interface to use (default: auto)"
+)
+@click.option("-c", "count", default=0, help="How many packets send (default: infinit)")
+@click.option("-t", "timeout", default=2, help="Timeout in seconds (default: 2)")
+@click.option(
+    "-w", "wait", default=1, help="How many seconds between packets (default: 1)"
+)
+@click.option("-v", "verbose", is_flag=True, default=False, help="Verbose")
+def cmd_icmp_ping6(ip, interface, count, timeout, wait, verbose):
+    """The classic ping tool that send ICMP echo requests (IPv6 version).
+
+    \b
+    # habu.icmp.ping6 dns.google.com
+    2001:4860:4860::8888 > 2800:810:598:916:6459:1fcd:b43e:31f2 IPv6 / ICMPv6 Echo Reply (id: 0x0 seq: 0x0)
+    2001:4860:4860::8888 > 2800:810:598:916:6459:1fcd:b43e:31f2 IPv6 / ICMPv6 Echo Reply (id: 0x0 seq: 0x1)
+    2001:4860:4860::8888 > 2800:810:598:916:6459:1fcd:b43e:31f2 IPv6 / ICMPv6 Echo Reply (id: 0x0 seq: 0x2)
+    2001:4860:4860::8888 > 2800:810:598:916:6459:1fcd:b43e:31f2 IPv6 / ICMPv6 Echo Reply (id: 0x0 seq: 0x3)
+    """
+
+    run_as_root()
+
+    if interface:
+        conf.iface = interface
+
+    conf.verb = False
+
+    counter = 0
+
+    layer3 = IPv6(dst=ip)
+    layer4 = ICMPv6EchoRequest()
+
+    while True:
+
+        layer4.seq = counter
+        pkt = layer3 / layer4
+
+        ans = sr1(pkt, timeout=timeout)
+        if ans:
+            if verbose:
+                ans.show()
+            else:
+                print(
+                    ans.sprintf(
+                        "IPv6 %IPv6.src% > %IPv6.dst% ICMPv6 seq=%ICMPv6EchoReply.seq%"
+                    )
+                )
+            del ans
+        else:
+            print("Timeout")
+
+        counter += 1
+
+        if count != 0 and counter == count:
+            break
+
+        sleep(wait)
+
+    return True
+
+
+if __name__ == "__main__":
+    cmd_icmp_ping6()
